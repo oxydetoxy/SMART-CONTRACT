383c8abace0d84c427c154f03a5704503e9cf3a2
root <root@Ishant.>
2025-02-03 14:39:48 +0000

    removed items

--- programs/nft/src/lib.rs
+++ programs/nft/src/lib.rs
@@ -6,11 +6,7 @@ declare_id!("AQurAZkuP6T5ZYB63seEECKByk9mXLG6jqaXToBhhcU4");
 pub mod nft {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
-}
+  
 
 #[derive(Accounts)]
 pub struct Initialize {}
